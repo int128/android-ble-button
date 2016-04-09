00e5c5f26039af188aa8458e05173c287d30c9cb
int128 <int128@gmail.com>
2016-04-09 14:01:04 +0900

    Grey out button on connecting

--- app/src/main/kotlin/org/hidetake/blebutton/MainActivity.kt
+++ app/src/main/kotlin/org/hidetake/blebutton/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.IBinder
 import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -22,24 +23,30 @@ class MainActivity : AppCompatActivity() {
             if (binder is BleService.BleServiceBinder) {
                 val bleContext = binder.bleContext
 
+                val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+                val frameAlert = findViewById(R.id.frame_alert) as FrameLayout
                 val buttonAlert = findViewById(R.id.button_alert) as ImageButton
+                val textLastTime = findViewById(R.id.text_last_time) as TextView
+                val textStatus = findViewById(R.id.text_ble_status) as TextView
+
                 buttonAlert.setOnClickListener { view ->
                     bleContext.immediateAlert()
                 }
 
-                val textLastTime = findViewById(R.id.text_last_time) as TextView
-                val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
                 bleContext.onButtonPressed {
                     vibrator.vibrate(300)
                     textLastTime.text = SimpleDateFormat.getDateTimeInstance().format(Date())
                 }
 
-                val textStatus = findViewById(R.id.text_ble_status) as TextView
                 bleContext.onConnecting {
+                    frameAlert.alpha = 0.2f
                     buttonAlert.isEnabled = false
                     textStatus.text = "Connecting to BLE device..."
                 }
+
                 bleContext.onIdle {
+                    frameAlert.alpha = 1.0f
                     buttonAlert.isEnabled = true
                     textStatus.text = "Connected to BLE device"
                 }
